c3d4c63d68ef21e24eae7c769fa78d2c07691817
Maksim Derkach <derckatch.mak@yandex.ru>
2024-04-20 23:15:22 +0300

    update

--- relabling.py
+++ relabling.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import sklearn
+import matplotlib.pyplot as plt
 
 
 class DatasetRelable:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dataset, label) -> None:
+        self.dataset = dataset
+        self.label = label
+
+        self.shape = self.dataset.drop(columns=self.label, axis=1).shape
+
+
+
+    
