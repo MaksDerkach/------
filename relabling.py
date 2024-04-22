824801d1f0abf4219c0fb8a012eb8f3dd7e6f802
Maksim Derkach <derckatch.mak@yandex.ru>
2024-04-22 21:20:48 +0300

    update

--- relabling.py
+++ relabling.py
@@ -18,27 +18,54 @@ class DatasetRelable:
                  data_mode: str='already_processed',
                  cat_cols: list=None, numeric_cols: list=None,
                  ordinal_cols: list=None, time_col: str=None,
-                 split_edges: list=[0.33, 0.66]) -> None:
+                 split_edges: list=[0.25, 0.5, 0.75]) -> None:
         
+        # dataset info
         self.dataset = dataset
         self.target_col = target_col
         self.data_mode = data_mode
-        
         self._columns = dataset.columns
 
+        # columns parameters
         self.cat_cols = cat_cols
         self.numeric_cols = numeric_cols
         self.time_col = time_col
 
-        self.split_edges = split_edges
+        self.raw_edges = split_edges
+        self.index_edges = self._get_index_edges(self.raw_edges)
+
+        self._edges_mapping = {0: 'D_init', 1: 'D_train',
+                               2: 'D_control_1', 3: 'D_control_2'}
+        
+
+
+    def _get_index_edges(self, edges):
+        edges = (np.array(edges) * self.dataset.shape[0]).astype(int)
+        return edges
+    
+
+    def _split_to_edges(self):
+        for n_group, sub_index in enumerate(np.split(self.dataset.index.to_numpy(), self.index_edges)):
+            self.dataset.loc[sub_index, 'split_group'] = self._edges_mapping[n_group]
+    
+
+    def _plot_split_data(self):
+        if self.time_col is not None:
+            sns.histplot(self.dataset, x=self.time_col,
+                         bins=self.dataset.shape[0] // 1000,
+                         hue='split_group')
+    
+    
+    def _get_split_statistics(self):
+        pass
 
 
     def __str__(self):
         return f"Data shape is {self.dataset.shape}, target column is '{self.target_col}'\n" +\
             f"Categorical columns: {self.cat_cols}\n" +\
             f"Numeric columns: {self.numeric_cols}\n" +\
-            f"Time columns is '{self.time_col}'\n" +\
-            f"Split edges: {self.split_edges}"
+            f"Time column is '{self.time_col}'\n" +\
+            f"Split edges: {self.raw_edges}"
 
 
     def auto_selection_dtypes(self):
@@ -81,25 +108,15 @@ class DatasetRelable:
         if self.time_col is not None:
             if mode == 'whole':
                 sns.histplot(self.dataset, x=self.time_col,
-                            bins=self.dataset.shape[0] // 1000,
-                            hue=self.target_col)
+                             bins=self.dataset.shape[0] // 1000,
+                             hue=self.target_col)
+                
             elif mode == 'edges':
-                pass
+                sns.histplot(self.dataset, x=self.time_col,
+                             bins=self.dataset.shape[0] // 1000,
+                             hue='split_group')
             else:
                 raise Exception(f"No such 'mode'='{mode}'")
         else:
             raise Exception("There is no 'time_col'")
 
-
-    def data_info(self):
-        pass
-
-    def basic_fit(self, test_size=0.2):
-        pass
-    
-    def pca(self):
-        pass
-
-    def set_pipeline(self):
-        pass
-    
