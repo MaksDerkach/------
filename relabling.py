6245158bc1565d20d20a40f34a6b25cec2be603c
Maksim Derkach <derckatch.mak@yandex.ru>
2024-06-04 00:13:52 +0300

    update

--- relabling.py
+++ relabling.py
@@ -83,7 +83,7 @@ class DatasetRelable:
         """
 
         if self.time_col is not None:
-            self.dataset = self.dataset.sort_values(self.time_col, ascending=True).reset_index(drop=True)
+            self.dataset = self.dataset.sort_values(self.time_col, ascending=True).reset_index(drop=True).copy()
             strategic = 'index'
         
         self.dataset[self._split_col] = Splitter(bounds, self._bounds_mapping, strategic).split(self.dataset) 
@@ -103,19 +103,21 @@ class DatasetRelable:
             plt.figure(figsize=(12, 4))
 
             if self.time_col_type == 'int':
-                sns.histplot(self.dataset, x=self.time_col,
-                             bins=self.dataset.shape[0] // 1000,
-                             hue=self._split_col, multiple="stack")
-                plt.title('Splitted data into 4 groups')
-                plt.ylabel('Number of samples')
-                
+                df_to_plot = self.dataset[[self.time_col, self._split_col]] \
+                                 .rename(columns={self._split_col: 'Subsets of data'})
+
+                sns.histplot(df_to_plot, x=self.time_col,
+                             bins=df_to_plot.shape[0] // 1000,
+                             hue='Subsets of data', multiple="stack")
+               
             elif self.time_col_type == 'datetime':
-                data_for_plot = self.dataset[[self.time_col, self._split_col, self.target_col]]
+                data_for_plot = self.dataset[[self.time_col, self._split_col, self.target_col]] \
+                                    .rename(columns={self._split_col: 'Subsets of data'})
                 data_for_plot[self.time_col] = pd.to_datetime(data_for_plot[self.time_col]).dt.strftime('%Y-%m-%d %Hh')
 
-                data_for_plot = data_for_plot.groupby([self.time_col, self._split_col]) \
+                data_for_plot = data_for_plot.groupby([self.time_col, 'Subsets of data']) \
                                              .agg(cnt_events=(self.target_col, 'count')).reset_index() \
-                                             .pivot(index=self.time_col, columns=self._split_col, values='cnt_events')
+                                             .pivot(index=self.time_col, columns='Subsets of data', values='cnt_events')
                 
                 step = data_for_plot.shape[0] // n_ticks
                 ticks = range(0, len(data_for_plot.index), step)
@@ -123,6 +125,10 @@ class DatasetRelable:
 
                 data_for_plot.plot.bar(stacked=True, figsize=(14, 4))
                 plt.xticks(ticks, labels, rotation=45)
+
+            plt.title('Splitted data into 4 groups')
+            plt.xlabel('Time axis')
+            plt.ylabel('Number of samples')
 
     
     def _get_split_statistics(self):
@@ -220,6 +226,8 @@ class DatasetRelable:
             y_labels = [f'{float(label.get_text())}%' for label in axes[i].yaxis.get_ticklabels()]
             axes[i].set_yticklabels(y_labels)
             axes[i].set_title(f"Target distribution score on '{sub}'")
+            axes[i].set_xlabel('Score')
+            axes[i].set_ylabel('Percent of data, %')
 
 
     def train_baseline(self, params: dict):
@@ -251,7 +259,7 @@ class DatasetRelable:
         print(f"Best params: {self.M_baseline.best_params_}")
     
 
-    def train_relabling(self, params: dict,
+    def train_matrix(self, params: dict,
                               TH_legetim: dict,
                               TH_fraud: dict):
         """
@@ -270,23 +278,27 @@ class DatasetRelable:
             for TH_f in np.arange(**TH_fraud):
                 print(f'Step {model_number} ... ')
 
-                data =  self._change_bounds(self.dataset.loc[inds, cols + [self.target_col]], TH_l, TH_f)
+                data = self._change_bounds(self.dataset.loc[inds, cols + [self.target_col]], TH_l, TH_f)
                 X = data.loc[inds, cols]
                 y = data.loc[inds, self._relable_target_col]
 
+                disbalance = y.sum() / y.count()
                 model = DummyModel(preprocessor=self.preprocessor.pipe, **params).fit(X, y)
 
+                # get statistics on validation subset
+                if (TH_l == 0) and (TH_f == 1):
+                    model = self.M_baseline
+                
                 # save current model
                 self._save_model(model, self._model_relabled_path, f'model_relabled_{model_number}')
 
-                # get statistics on validation subset
                 roc_auc, ap = self._model_scores(model, 'D_control_1')
-                self.TH_matrix.append([f'model_relabled_{model_number}', TH_l, TH_f, roc_auc, ap])
+                self.TH_matrix.append([f'model_relabled_{model_number}', TH_l, TH_f, roc_auc, ap, disbalance])
 
                 model_number += 1
         
         # convert matrix to DataFrame
-        self.TH_matrix = pd.DataFrame(self.TH_matrix, columns=['model_name', 'TH_l', 'TH_f', 'ROC_AUC', 'AP'])
+        self.TH_matrix = pd.DataFrame(self.TH_matrix, columns=['model_name', 'TH_l', 'TH_f', 'ROC_AUC', 'AP', 'Disbalance'])
 
         
 
@@ -314,7 +326,7 @@ class DatasetRelable:
 
         return roc_auc, ap
     
-    def scores_curve_display(self, model: str,
+    def display_model_curves(self, model: str,
                                    test_subset: str,
                                    train_subset: str=None,
                                    figsize: tuple=(15, 5)):
@@ -323,7 +335,8 @@ class DatasetRelable:
             - 'relabler'
             - 'baseline'
         """
-        names = ['train', 'test']
+        names = {key: value for key, value in zip(['Score on train', 'Score on test'],
+                                                  [train_subset, test_subset]) if value is not None}
 
         if model == 'Relabler':
                 model_ = self.M_relabler
@@ -333,7 +346,7 @@ class DatasetRelable:
         _, axes = plt.subplots(1, 2, figsize=figsize)
         plt.suptitle(f"Scores curves for '{model}'")
 
-        for name, subset in zip(names, [train_subset, test_subset]):
+        for name, subset in names.items():
 
             inds, cols = self._prepare_index(subset)
             X = self.dataset.loc[inds, cols]
@@ -354,20 +367,38 @@ class DatasetRelable:
                 axes[i].set_title(title)
 
 
-    def plot_TH_matrix(self, cmap='BuGn', figsize: tuple=(15, 5), fmt='.3g', fontsize=10):
+    def plot_TH_matrix(self, cmaps=['BuGn', 'RdYlGn'], figsize: tuple=(15, 5),
+                       fmt: list=['.3f', '.3f'], with_relative: bool=False):
         """
         Plot heatmap based on various thresholds TH_l and TH_f after training model on relabled data `D_train`
         """
-        metrics = ['ROC_AUC', 'AP']
+        title = 'Main metrics with different thresholds'
+        title_relative = 'Difference of metrics between model with relabling and Baseline'
 
-        fig, axes = plt.subplots(1, len(metrics), figsize=figsize)
-        plt.suptitle('Main metrics with different thresholds')
+        self._plot_TH_matrix([0, 0], cmaps[0], False, title, figsize, fmt)
+        
+        if with_relative:
+            relative_values = self._model_scores(self.M_baseline, 'D_control_1')[::-1]
+            self._plot_TH_matrix(relative_values, cmaps[1], True, title_relative, figsize, fmt)
+
+
+    def _plot_TH_matrix(self, relative_values: list, cmap, is_relative, title, figsize, fmt):
+        """
+        """
+        metrics = ['AP', 'ROC_AUC']
+
+        _, axes = plt.subplots(1, len(metrics), figsize=figsize)
+        plt.suptitle(title)
 
         for i, metric in enumerate(metrics):
-            scores = self.TH_matrix.pivot(index='TH_l', columns='TH_f', values=metric)
-            sns.heatmap(scores.sort_index(ascending=False),
-                        annot=True, cmap=cmap, fmt=fmt, ax=axes[i],
-                        annot_kws={"fontsize":fontsize})
+            scores = self.TH_matrix.pivot(index='TH_l', columns='TH_f', values=metric) - relative_values[i]
+
+            if is_relative:
+                sns.heatmap(scores.sort_index(ascending=False),
+                            annot=True, cmap=cmap, center=0, fmt=fmt[i], ax=axes[i])
+            else:
+                sns.heatmap(scores.sort_index(ascending=False),
+                            annot=True, cmap=cmap, fmt=fmt[i], ax=axes[i])
             
             x_labels = [f'{float(label.get_text()):.2f}' for label in axes[i].xaxis.get_ticklabels()]
             y_labels = [f'{float(label.get_text()):.2f}' for label in axes[i].yaxis.get_ticklabels()]
@@ -378,24 +409,78 @@ class DatasetRelable:
             axes[i].set_title(f'TH matrix for {metric}')
 
 
-
-    def compare_models(self, main_metric, subset='D_control_2'):
+    def compare_models(self, method, main_metric, thresh=None, show_curve: bool=False, figsize: tuple=(15, 5)):
         """
-        Compare statistics of M_b and M_s models on `subset`. Default subset='D_control_2'
+        Compare statistics of M_b and M_s models 'D_control_2'
+
+        Parameters
+        ----------
+        `method`: two possible values:
+            - auto
+            - exact
 
         `main_metric`: two possible values:
             - 'AP'
             - 'ROC_AUC'
         """
-        model_to_compare, TH_l, TH_f = self._choose_best(main_metric)
+        if method == 'auto':
+            model_to_compare, TH_l, TH_f, disbalance = self._choose_best(main_metric)
 
-        for model, descr in zip([self.M_baseline, model_to_compare],
-                                ['Baseline', f'Baseline with TH_l={TH_l:.3f} and TH_f={TH_f:.3f}']):
-            roc_auc, ap = self._model_scores(model, subset)
+        elif method == 'exact':
+            TH_l, TH_f = thresh
+            chosed_model = self.TH_matrix[(self.TH_matrix.TH_l.apply(lambda x: f"{x:.3f}") == f"{TH_l:.3f}") 
+                                          & (self.TH_matrix.TH_f.apply(lambda x: f"{x:.3f}") == f"{TH_f:.3f}")]
+
+            model_to_compare = load(self._model_relabled_path + f'/{chosed_model.model_name.values[0]}.joblib')
+            disbalance = chosed_model.Disbalance.values[0]
+
+        models = [self.M_baseline, model_to_compare]
+
+        disbalance_list = [self.dataset[self.dataset[self._split_col] == 'D_train'][self.target_col].sum() \
+                                / self.dataset[self.dataset[self._split_col] == 'D_train'][self.target_col].count(),
+                           disbalance]
+
+        text_for_legend = ['Baseline', f'Baseline with TH_l={TH_l:.3f} and TH_f={TH_f:.3f}']
+
+        for model, descr, disb in zip(models, text_for_legend, disbalance_list):
+            roc_auc, ap = self._model_scores(model, 'D_control_2')
 
             print(f"Model: {descr}")
+            print(f"Disbalance on 'D_train': {(disb * 100):.3f}%\n")
+
+            print("Metrics on 'D_control_2':")
             print(f'ROC_AUC: {roc_auc:.5f}')
             print(f'AP: {ap:.5f}\n')
+        
+        if show_curve:
+            self._compare_models_curves(models, 'D_control_2',
+                                        ['Baseline', 'Baseline with TH-relabling'], figsize)
+
+
+    def _compare_models_curves(self, models, subset, text_for_legend, figsize: tuple=(15, 5)):
+        """
+        """
+        _, axes = plt.subplots(1, 2, figsize=figsize)
+        plt.suptitle('Comparison of the main metrics for the models')
+
+        for model, text in zip(models, text_for_legend):
+
+            inds, cols = self._prepare_index(subset)
+            X = self.dataset.loc[inds, cols]
+            y = self.dataset.loc[inds, self.target_col]
+            y_proba = model.predict_proba(X)[:, 1]
+
+            for i, data in enumerate(zip([PrecisionRecallDisplay, RocCurveDisplay],
+                                         ['best', 'lower right'],
+                                         ["Precision-Recall curve", "ROC-AUC curve"])):
+                
+                metric_curve, position, title = data
+                __ = metric_curve.from_predictions(y, y_proba,
+                                                   name=f'{text}',
+                                                   ax=axes[i])
+
+                axes[i].legend(loc=f'{position}')
+                axes[i].set_title(title)
 
 
 
@@ -404,8 +489,9 @@ class DatasetRelable:
 
         TH_l, TH_f = best_result.TH_l.values[0], best_result.TH_f.values[0]
         best_model = load(self._model_relabled_path + f'/{best_result.model_name.values[0]}.joblib')
+        disbalance = best_result.Disbalance.values[0]
 
-        return best_model, TH_l, TH_f
+        return best_model, TH_l, TH_f, disbalance
     
 
     def _save_model(self, object, path, name):
